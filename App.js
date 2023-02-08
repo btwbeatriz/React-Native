08ddc7bc76494ac5f0fd69137d1c5bdbf974f26a
btwbeatriz <rm95246@fiap.com.br>
2023-02-08 20:28:02 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, TextInput, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function Principal(){
+
+  let position = {"textAlign": "center"}
+
+  return(
+    <View style={ position }>
+      <Text>AuMiau</Text>
+      <Text><hr></hr></Text>
+      <Text>Nome do Pet</Text>
+      <TextInput/>
+      <Text>Raça</Text>
+      <TextInput/>
+      <Text>Peso</Text>
+      <TextInput/>
+      <Text>Nascimento</Text>
+      <TextInput/>
+      <Text>Nome do Dono</Text>
+      <TextInput/>
+      <Button
+        title="Cadastrar"
+        color="#008b8b"
+        onPress={() => alert('Cadastro realizado!')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Principal;
